bb4e0112e3683fd56ea4923fefa9279205a9475f
MarieLou <timcke@hotmail.de>
2016-07-23 05:26:21 +0200

    infografiken projekt

---
+++ _site/js/tooltip.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.tooltip').tooltipster({
+    theme: 'tooltipster-punk',
+    delay: 0,
+    timer: 20000,
+    delayTouch: 0,
+    'maxWidth': 270,
+    contentAsHTML: true,
+    touchDevices: true,
+    interactive: true,
+    trigger: 'hover'
+    //   trigger: 'custom',
+    // triggerOpen: {
+      //   click: true,
+      //   touchstart: true
+      //   },
+      //  triggerClose: {
+      //   click: true,
+      //   scroll: false,
+      //   tap: true
+      //   }
+});
+});
